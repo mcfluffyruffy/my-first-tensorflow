607c0b2088df48e3f39c4371e3232aa9ab5784ea
mcfluffyruffy <erind.ujkani@gmail.com>
2024-04-07 21:15:59 +0200

    Change to opencv R PI camera capture and store

---
+++ my-first-detector.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+
+
+// Constants.
+const float INPUT_WIDTH = 640.0;
+const float INPUT_HEIGHT = 640.0;
+const float SCORE_THRESHOLD = 0.5;
+const float NMS_THRESHOLD = 0.45;
+const float CONFIDENCE_THRESHOLD = 0.45;
+ 
+// Text parameters.
+const float FONT_SCALE = 0.7;
+const int FONT_FACE = cv::FONT_HERSHEY_SIMPLEX;
+const int THICKNESS = 1;
+ 
+// Colors.
+cv::Scalar BLACK = cv::Scalar(0,0,0);
+cv::Scalar BLUE = cv::Scalar(255, 178, 50);
+cv::Scalar YELLOW = cv::Scalar(0, 255, 255);
+cv::Scalar RED = cv::Scalar(0,0,255);
+
+
+void DrawLabel(cv::Mat& inputImage, std::string label, int left, int top)
+{
+    // Display the label at the top of the bounding box.
+    int baseLine;
+    cv::Size label_size = cv::getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS, &baseLine);
+    top = std::max(top, label_size.height);
+    // Top left corner.
+    cv::Point tlc = cv::Point(left, top);
+    // Bottom right corner.
+    cv::Point brc = cv::Point(left + label_size.width, top + label_size.height + baseLine);
+    // Draw white rectangle.
+    cv::rectangle(inputImage, tlc, brc, BLACK);
+    // Put the label on the black rectangle.
+    putText(inputImage, label, cv::Point(left, top + label_size.height), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS);
+}
+
+std::vector<cv::Mat> PreProcess(cv::Mat &inputImage, cv::dnn::Net &net)
+{
+    // Convert to blob.
+    cv::Mat blob;
+    cv::dnn::blobFromImage(inputImage, blob, 1./255., cv::Size(INPUT_WIDTH, INPUT_HEIGHT), cv::Scalar(), true, false);
+ 
+    net.setInput(blob);
+ 
+    // Forward propagate.
+    std::vector<cv::Mat> outputs;
+    net.forward(outputs, net.getUnconnectedOutLayersNames());
+ 
+    return outputs;
+}
+
+int main() {
+    std::stringstream ss;
+    ss << "nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=2 ! video/x-raw, width=480, height=680, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+
+    cv::VideoCapture video;
+
+    video.open(ss.str());
+
+    if (!video.isOpened())
+    {
+        std::cout << "Unable to get video from the camera!" << std::endl;
+
+        return -1;
+    }
+
+    cv::Mat frame;
+
+    video.read(frame);
+
+    std::cout << "Finished!" << std::endl;
+    cv::imwrite("test.jpg",frame);
+    return 0;
+}
